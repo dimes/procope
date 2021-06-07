8907790d713e670e95ddb917e4277139a4dff434
dimes <mgale4@binghamton.edu>
2021-06-06 21:51:42 -0400

    Prod config

--- viewer/src/config.ts
+++ viewer/src/config.ts
@@ -1,8 +1,15 @@
 import { config as fclConfig } from '@onflow/fcl';
 
 export function init() {
+  /*
   fclConfig()
     .put('accessNode.api', 'http://localhost:8080')
     .put('challenge.handshake', 'http://localhost:3000/fcl/authn')
+    .put('0xProfile', '0x01cf0e2f2f715450');
+  */
+
+  fclConfig()
+    .put('accessNode.api', 'https://access-testnet.onflow.org')
+    .put('challenge.handshake', 'https://fcl-discovery.onflow.org/testnet/authn')
     .put('0xProfile', '0x01cf0e2f2f715450');
 }
